af0d60520148a2486ef3cffb7d6ae92280ff8927
Florian <FlorBl>
2021-11-19 11:30:32 -0500

    Commie

--- project3/settings.py
+++ project3/settings.py
@@ -14,10 +14,9 @@ import os
 import django_heroku
 import dj_database_url
 from decouple import config
-from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
